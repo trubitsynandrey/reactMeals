577982eda48d51afc5615ccc3a73d82f0ac8aee8
Andrey Trubitsyn <andrejtrubicyn@Andrejs-MacBook-Air.local>
2022-02-10 15:42:21 +0400

    add: implement header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
+import { Header } from "./components/Layout/Header";
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
